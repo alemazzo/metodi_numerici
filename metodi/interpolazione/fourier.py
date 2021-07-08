8e814c4fb64ac9e273b9cde84a4254641777c449
Alessandro Mazzoli <alemazzoli97@gmail.com>
2021-07-08 14:24:18 +0200

    added documentation to fourier

--- metodi/interpolazione/fourier.py
+++ metodi/interpolazione/fourier.py
@@ -5,12 +5,15 @@ from scipy.fftpack import fftshift, ifftshift
 
 def fourier(f, t, fs, filtro):
     """
+    Filtraggio di un segnale che presenta un rumore basandosi su un filtro
+    sulle sequenze specificato.
 
-    :param f:
-    :param t:
-    :param fs:
-    :param filtro:
-    :return:
+    :param f: la funzione composta dalla funzione esatta sommata alla funzione rumore
+    :param t: durata del segnale
+    :param fs: frequenza di campionamento
+    :param filtro: il filtro da applicare alle frequenze (lambda in funzione dell'array delle frequenze
+                   [lambda freq: np.abs(freq) > 10])
+    :return: i valori della funzione filtrata nel linespace da 0 a t con t * fs punti
     """
     d = 1 / fs
     n = t * fs
@@ -19,11 +22,10 @@ def fourier(f, t, fs, filtro):
     x = np.linspace(0, t, n)
     y = f(x)
 
-    frequenze = np.arange(-fs/2, fs/2, delta)
+    frequenze = np.arange(-fs / 2, fs / 2, delta)
     polinomio = fftshift(fft(y))
 
     indici = filtro(frequenze)
     polinomio[indici] = 0
 
     return ifft(ifftshift(polinomio))
-
